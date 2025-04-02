7c7a3dce7f6778e78a1b8d34eee594e2b2abf965
pataegrillo <javier@contimita.com>
2025-04-02 11:19:01 -0400

    - Now the decoder can parse dogecoin blocks

--- src/api.js
+++ src/api.js
@@ -21,10 +21,11 @@ const DECODER_DB_NAME =  process.env.DECODER_DB_NAME
 const DECODER_DB_USER =  process.env.DECODER_DB_USER
 const DB_PASSWORD =  process.env.DECODER_DB_PASS
 const DECODER_API_PORT = process.env.DECODER_API_PORT
+const AUX_POW = process.env.AUX_POW
 
 async function startApi(){
 	//Start the indexer
-	const decoder = new XChainDecoder(NETWORK, DB_URL, DB_PORT, DECODER_DB_NAME, DECODER_DB_USER, DB_PASSWORD, NODE_URL, NODE_PORT, NODE_USER, NODE_PASSWORD);
+	const decoder = new XChainDecoder(NETWORK, DB_URL, DB_PORT, DECODER_DB_NAME, DECODER_DB_USER, DB_PASSWORD, NODE_URL, NODE_PORT, NODE_USER, NODE_PASSWORD, AUX_POW);
 	decoder.start()
 
 	// Create the app
