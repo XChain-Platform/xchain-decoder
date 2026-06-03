7d837b6ff8da16ddeb4555c883be892a93d3c270
J-Dog <j-dog@j-dog.net>
2026-06-02 22:26:12 -0700

    Normalize pubkeys.sql to standard DROP+CREATE reset pattern

--- src/sql/pubkeys.sql
+++ src/sql/pubkeys.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS pubkeys (
+DROP TABLE IF EXISTS pubkeys;
+CREATE TABLE pubkeys (
   address_id INTEGER UNSIGNED NOT NULL PRIMARY KEY,
   pubkey     VARCHAR(66) NOT NULL,
   FOREIGN KEY (address_id) REFERENCES index_addresses(id)
